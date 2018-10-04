147dbad950dbf1163ae9270a1e6378394a794bbc
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 06:06:45 +0900

    add Hobby validation

--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -2,4 +2,9 @@ class Hobby < ApplicationRecord
   has_many :user_hobbies
   has_many :users, through: :user_hobbies
 
+  validates:title,
+    presence: true,
+    length: { maximum:40 }, 
+    uniqueness: true
+
 end
