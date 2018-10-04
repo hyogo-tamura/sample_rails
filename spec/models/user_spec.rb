f0f09d09d565308b47d30165af2eda5a1d284760
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 06:34:22 +0900

    add spec User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  it 'is valid with email' do
+    pass = Faker::Internet.password(8)
+    user = User.new(email: Faker::Internet.email, password: pass, password_confirmation: pass)
+    expect(user).to be_valid
+  end
+
+  it 'is valid with email and username' do
+    pass = Faker::Internet.password(8)
+    user = User.new(email: Faker::Internet.email, username: Faker::Internet.username, password: pass, password_confirmation: pass)
+    expect(user).to be_valid
+  end
+
+  it 'is NOT valid when username is too long' do
+    pass = Faker::Internet.password(8)
+    user = User.new(email: Faker::Internet.email, username: 'the very verrrrrrrrrry long usename, maybe he want to fill my storage', password: pass, password_confirmation: pass)
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid when password confirmation is different' do
+    pass = 'password'
+    pass_c = 'different_password'
+    user = User.new(email: Faker::Internet.email, password: pass, password_confirmation: pass_c)
+    expect(user).to_not be_valid
+  end
+
 end
