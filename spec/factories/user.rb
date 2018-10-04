087d19b19ac7c008c83b326e35dc7d6ae9ea45e7
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 06:33:06 +0900

    replace deprecated methods

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   pass = Faker::Internet.password(8)
 
   factory :user do
-    email                 Faker::Internet.email
-    password              pass
-    password_confirmation pass
+    username { Faker::Internet.name }
+    email { Faker::Internet.email }
+    password { pass }
+    password_confirmation { pass }
   end
 end
