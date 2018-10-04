f168fe2d2cbdd16e8aaaeaa65673e9bc06c02c1d
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 06:07:23 +0900

    delete rspec pages#show

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -33,11 +33,4 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
