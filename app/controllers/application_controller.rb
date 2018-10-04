5c10c3e3a828ccd431e761fafc239666359c83f0
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 01:11:47 +0900

    add form for username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:username, :password, :password_confirmation, :email)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :password, :remember_me, :email)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :password, :current_password, :email)
+    end
+  end 
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :show
+  
   def index
   end
 
   def show
+    
   end
 end
