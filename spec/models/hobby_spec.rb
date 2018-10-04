d399655d3e635f96a3e91c35b0a1a0a58aa8e557
Hyogo Tamura <ochaaa@MacBook-Air.local>
2018-10-05 06:07:40 +0900

    add spec Hobby

--- spec/models/hobby_spec.rb
+++ spec/models/hobby_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Hobby, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with title' do
+    hobby = Hobby.new(title: 'test')
+    expect(hobby).to be_valid
+  end
+
+  it 'is invalid without title' do
+    hobby = Hobby.new()
+    expect(hobby).to_not be_valid
+  end
+
+  it 'is invalid with blank title' do
+    hobby = Hobby.new(title: ' ')
+    expect(hobby).to_not be_valid
+  end
+
+  it 'is not valid with long title' do
+    hobby = Hobby.new(title: 'this is a very long title for the hobby that too long to tell us what you like.')
+    expect(hobby).to_not be_valid
+  end
+
+  describe 'when the title is already taken' do
+    before {
+      hobby = Hobby.new(title: 'test')
+      hobby.save!
+    }
+    it 'is not valid' do
+      hobby2 = Hobby.new(title: 'test')
+      expect(hobby2).to_not be_valid
+    end
+  end
 end
